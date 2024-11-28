29125d9a62ac69e9f17d908d692e9081a4987670
rarfar <ryan.mcgregor@mail.mcgill.ca>
2024-11-27 22:52:46 -0500

    winning but too much time

--- agents/__init__.py
+++ agents/__init__.py
@@ -2,3 +2,4 @@ from .random_agent import RandomAgent
 from .human_agent import HumanAgent
 from .student_agent import StudentAgent
 from .gpt_greedy_corners_agent import StudentAgent
+from .student_agent3 import StudentAgent
